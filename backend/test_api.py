6f575bfffbf2f39568574b446cd7877e3d556397
Yash Patni <ypatni3@gmail.com>
2021-07-29 22:54:37 -0400

    completed tests for functions in get_handler and api

--- backend/test_api.py
+++ backend/test_api.py
@@ -8,3 +8,30 @@ def test_get_ticket_count(mocker: MockerFixture) -> None:
     assert ZendeskApiCallHelper.get_total_tickets_count() == mock_response
     assert mocked_function.call_count == 1
     assert mocked_function.call_args[0][0] == "https://zcctest.zendesk.com/api/v2/tickets/count"
+
+def test_get_ticket(mocker: MockerFixture) -> None:
+    mock_response = mocker.MagicMock()
+    mocked_function = mocker.patch.object(ZendeskApiCallHelper, "_get_api_call", return_value=mock_response)
+    assert ZendeskApiCallHelper.get_ticket(3) == mock_response
+    assert mocked_function.call_count == 1
+    assert mocked_function.call_args[0][0] == "https://zcctest.zendesk.com/api/v2/tickets/3"
+
+def test_get_tickets(mocker: MockerFixture) -> None:
+    mock_response_count = mocker.MagicMock()
+    mock_response_api_call = mocker.MagicMock()
+    mocked_function = mocker.patch.object(ZendeskApiCallHelper, "get_total_tickets_count", return_value=mock_response_count)
+    mocked_api_function = mocker.patch.object(ZendeskApiCallHelper, "_get_api_call", return_value=mock_response_api_call)
+    assert ZendeskApiCallHelper.get_total_tickets_count() == mock_response_count
+    assert ZendeskApiCallHelper.get_tickets('mock_call')['tickets'] == mock_response_api_call
+    assert ZendeskApiCallHelper.get_tickets('mock_call')['info'] == mock_response_count
+    assert mocked_api_function.call_args[0][0] == 'mock_call'
+
+
+def test_get_tickets(mocker: MockerFixture) -> None:
+    mock_response_count = mocker.MagicMock()
+    mock_response_api_call = mocker.MagicMock()
+    mocked_function = mocker.patch.object(ZendeskApiCallHelper, "get_total_tickets_count", return_value=mock_response_count)
+    mocked_api_function = mocker.patch.object(ZendeskApiCallHelper, "_get_api_call", return_value=mock_response_api_call)
+    assert ZendeskApiCallHelper.get_total_tickets_count() == mock_response_count
+    assert ZendeskApiCallHelper.get_tickets(None)['tickets'] == mock_response_api_call
+    assert mocked_api_function.call_args[0][0] == 'https://zcctest.zendesk.com/api/v2/tickets.json?page[size]=25'
--- backend/test_handler.py
+++ backend/test_handler.py
@@ -1,20 +1,138 @@
 from handler import get_tickets
 from handler import get_ticket
+from pytest_mock import MockerFixture
+from api import ZendeskApiCallHelper
+
 import json
 
-def test_get_tickets_handler():
+def test_get_tickets_handler_error(mocker: MockerFixture) -> None:
+    mock_response = mocker.MagicMock()
+    mocked_function = mocker.patch.object(ZendeskApiCallHelper, "get_tickets", return_value=mock_response)
     response = get_tickets(
         {
-            'body': json.dumps({"cursor": "https://zcctest.zendesk.com/api/v2/tickets.json?page%5Bafter%5D=eyJvIjoibmljZV9pZCIsInYiOiJhUmtBQUFBQUFBQUEifQ%3D%3D&page%5Bsize%5D=25"
-            })
+            'body': json.dumps({"cursor": 'mock cursor'})
         }, {}
     )
-    assert response["statusCode"] == 200
+    assert mocked_function.call_count == 1
+    assert response == {'statusCode': 500, 'headers': {'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Credentials': True}, 'body': '"Object of type MagicMock is not JSON serializable"'}
 
-def test_get_ticket_by_id_handler():
-    event = {'resource': '/tickets/{id}', 'path': '/tickets/1', 'httpMethod': 'GET', 'headers': {'Accept': '*/*', 'Accept-Encoding': 'gzip, deflate, br', 'Accept-Language': 'en-US,en;q=0.9', 'CloudFront-Forwarded-Proto': 'https', 'CloudFront-Is-Desktop-Viewer': 'true', 'CloudFront-Is-Mobile-Viewer': 'false', 'CloudFront-Is-SmartTV-Viewer': 'false', 'CloudFront-Is-Tablet-Viewer': 'false', 'CloudFront-Viewer-Country': 'US', 'Host': 'faq1slxbph.execute-api.us-east-1.amazonaws.com', 'origin': 'http://localhost:3000', 'Referer': 'http://localhost:3000/', 'sec-ch-ua': '" Not;A Brand";v="99", "Google Chrome";v="91", "Chromium";v="91"', 'sec-ch-ua-mobile': '?0', 'sec-fetch-dest': 'empty', 'sec-fetch-mode': 'cors', 'sec-fetch-site': 'cross-site', 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36', 'Via': '2.0 27f5831be5a9ad411fca9c84fe627bdc.cloudfront.net (CloudFront)', 'X-Amz-Cf-Id': '31c5JvO6KQbQjpWEdc5Rey0VW6PvAO8g1OhX6YSkVV4kZZseAeXoEw==', 'X-Amzn-Trace-Id': 'Root=1-6101a0bb-31d3485b54b5fd8866015641', 'X-Forwarded-For': '76.124.168.93, 52.46.46.111', 'X-Forwarded-Port': '443', 'X-Forwarded-Proto': 'https'}, 'multiValueHeaders': {'Accept': ['*/*'], 'Accept-Encoding': ['gzip, deflate, br'], 'Accept-Language': ['en-US,en;q=0.9'], 'CloudFront-Forwarded-Proto': ['https'], 'CloudFront-Is-Desktop-Viewer': ['true'], 'CloudFront-Is-Mobile-Viewer': ['false'], 'CloudFront-Is-SmartTV-Viewer': ['false'], 'CloudFront-Is-Tablet-Viewer': ['false'], 'CloudFront-Viewer-Country': ['US'], 'Host': ['faq1slxbph.execute-api.us-east-1.amazonaws.com'], 'origin': ['http://localhost:3000'], 'Referer': ['http://localhost:3000/'], 'sec-ch-ua': ['" Not;A Brand";v="99", "Google Chrome";v="91", "Chromium";v="91"'], 'sec-ch-ua-mobile': ['?0'], 'sec-fetch-dest': ['empty'], 'sec-fetch-mode': ['cors'], 'sec-fetch-site': ['cross-site'], 'User-Agent': ['Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36'], 'Via': ['2.0 27f5831be5a9ad411fca9c84fe627bdc.cloudfront.net (CloudFront)'], 'X-Amz-Cf-Id': ['31c5JvO6KQbQjpWEdc5Rey0VW6PvAO8g1OhX6YSkVV4kZZseAeXoEw=='], 'X-Amzn-Trace-Id': ['Root=1-6101a0bb-31d3485b54b5fd8866015641'], 'X-Forwarded-For': ['76.124.168.93, 52.46.46.111'], 'X-Forwarded-Port': ['443'], 'X-Forwarded-Proto': ['https']}, 'queryStringParameters': None, 'multiValueQueryStringParameters': None, 'pathParameters': {'id': '1'}, 'stageVariables': None, 'requestContext': {'resourceId': 'ciwo05', 'resourcePath': '/tickets/{id}', 'httpMethod': 'GET', 'extendedRequestId': 'DMYNTHvQIAMFV7g=', 'requestTime': '28/Jul/2021:18:23:55 +0000', 'path': '/dev/tickets/1', 'accountId': '689157321639', 'protocol': 'HTTP/1.1', 'stage': 'dev', 'domainPrefix': 'faq1slxbph', 'requestTimeEpoch': 1627496635393, 'requestId': '5d8d4dbd-d712-40a6-8a64-a6a1f5f6450d', 'identity': {'cognitoIdentityPoolId': None, 'accountId': None, 'cognitoIdentityId': None, 'caller': None, 'sourceIp': '76.124.168.93', 'principalOrgId': None, 'accessKey': None, 'cognitoAuthenticationType': None, 'cognitoAuthenticationProvider': None, 'userArn': None, 'userAgent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36', 'user': None}, 'domainName': 'faq1slxbph.execute-api.us-east-1.amazonaws.com', 'apiId': 'faq1slxbph'}, 'body': None, 'isBase64Encoded': False}
-    response = get_ticket(
-        event, {}
+def test_get_tickets_handler_success(mocker: MockerFixture) -> None:
+    mocked_function = mocker.patch.object(ZendeskApiCallHelper, "get_tickets", return_value={})
+    response = get_tickets(
+        {
+            'body': json.dumps({"cursor": 'mock cursor'})
+        }, {}
     )
-    assert response['statusCode'] == 200
+    assert mocked_function.call_count == 1
+    assert response == {'body': '{}',
+ 'headers': {'Access-Control-Allow-Credentials': True,
+             'Access-Control-Allow-Origin': '*'},
+ 'statusCode': 200}
 
+def test_get_ticket_success(mocker: MockerFixture) -> None:
+    mocked_function = mocker.patch.object(ZendeskApiCallHelper, "get_ticket", return_value={})
+    response = get_ticket(
+        {'resource': '/tickets/{id}', 'path': '/tickets/1', 'httpMethod': 'GET',
+         'headers': {'Accept': '*/*', 'Accept-Encoding': 'gzip, deflate, br', 'Accept-Language': 'en-US,en;q=0.9',
+                     'CloudFront-Forwarded-Proto': 'https', 'CloudFront-Is-Desktop-Viewer': 'true',
+                     'CloudFront-Is-Mobile-Viewer': 'false', 'CloudFront-Is-SmartTV-Viewer': 'false',
+                     'CloudFront-Is-Tablet-Viewer': 'false', 'CloudFront-Viewer-Country': 'US',
+                     'Host': 'faq1slxbph.execute-api.us-east-1.amazonaws.com', 'origin': 'http://localhost:3000',
+                     'Referer': 'http://localhost:3000/',
+                     'sec-ch-ua': '" Not;A Brand";v="99", "Google Chrome";v="91", "Chromium";v="91"',
+                     'sec-ch-ua-mobile': '?0', 'sec-fetch-dest': 'empty', 'sec-fetch-mode': 'cors',
+                     'sec-fetch-site': 'cross-site',
+                     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36',
+                     'Via': '2.0 27f5831be5a9ad411fca9c84fe627bdc.cloudfront.net (CloudFront)',
+                     'X-Amz-Cf-Id': '31c5JvO6KQbQjpWEdc5Rey0VW6PvAO8g1OhX6YSkVV4kZZseAeXoEw==',
+                     'X-Amzn-Trace-Id': 'Root=1-6101a0bb-31d3485b54b5fd8866015641',
+                     'X-Forwarded-For': '76.124.168.93, 52.46.46.111', 'X-Forwarded-Port': '443',
+                     'X-Forwarded-Proto': 'https'},
+         'multiValueHeaders': {'Accept': ['*/*'], 'Accept-Encoding': ['gzip, deflate, br'],
+                               'Accept-Language': ['en-US,en;q=0.9'], 'CloudFront-Forwarded-Proto': ['https'],
+                               'CloudFront-Is-Desktop-Viewer': ['true'], 'CloudFront-Is-Mobile-Viewer': ['false'],
+                               'CloudFront-Is-SmartTV-Viewer': ['false'], 'CloudFront-Is-Tablet-Viewer': ['false'],
+                               'CloudFront-Viewer-Country': ['US'],
+                               'Host': ['faq1slxbph.execute-api.us-east-1.amazonaws.com'],
+                               'origin': ['http://localhost:3000'], 'Referer': ['http://localhost:3000/'],
+                               'sec-ch-ua': ['" Not;A Brand";v="99", "Google Chrome";v="91", "Chromium";v="91"'],
+                               'sec-ch-ua-mobile': ['?0'], 'sec-fetch-dest': ['empty'], 'sec-fetch-mode': ['cors'],
+                               'sec-fetch-site': ['cross-site'], 'User-Agent': [
+                 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36'],
+                               'Via': ['2.0 27f5831be5a9ad411fca9c84fe627bdc.cloudfront.net (CloudFront)'],
+                               'X-Amz-Cf-Id': ['31c5JvO6KQbQjpWEdc5Rey0VW6PvAO8g1OhX6YSkVV4kZZseAeXoEw=='],
+                               'X-Amzn-Trace-Id': ['Root=1-6101a0bb-31d3485b54b5fd8866015641'],
+                               'X-Forwarded-For': ['76.124.168.93, 52.46.46.111'], 'X-Forwarded-Port': ['443'],
+                               'X-Forwarded-Proto': ['https']}, 'queryStringParameters': None,
+         'multiValueQueryStringParameters': None, 'pathParameters': {'id': '1'}, 'stageVariables': None,
+         'requestContext': {'resourceId': 'ciwo05', 'resourcePath': '/tickets/{id}', 'httpMethod': 'GET',
+                            'extendedRequestId': 'DMYNTHvQIAMFV7g=', 'requestTime': '28/Jul/2021:18:23:55 +0000',
+                            'path': '/dev/tickets/1', 'accountId': '689157321639', 'protocol': 'HTTP/1.1',
+                            'stage': 'dev', 'domainPrefix': 'faq1slxbph', 'requestTimeEpoch': 1627496635393,
+                            'requestId': '5d8d4dbd-d712-40a6-8a64-a6a1f5f6450d',
+                            'identity': {'cognitoIdentityPoolId': None, 'accountId': None, 'cognitoIdentityId': None,
+                                         'caller': None, 'sourceIp': '76.124.168.93', 'principalOrgId': None,
+                                         'accessKey': None, 'cognitoAuthenticationType': None,
+                                         'cognitoAuthenticationProvider': None, 'userArn': None,
+                                         'userAgent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36',
+                                         'user': None}, 'domainName': 'faq1slxbph.execute-api.us-east-1.amazonaws.com',
+                            'apiId': 'faq1slxbph'}, 'body': None, 'isBase64Encoded': False}, {}
+    )
+    assert mocked_function.call_count == 1
+    assert response == {'body': '{}',
+                        'headers': {'Access-Control-Allow-Credentials': True,
+                                    'Access-Control-Allow-Origin': '*'},
+                        'statusCode': 200}
+
+def test_get_ticket_success(mocker: MockerFixture) -> None:
+    mock_response = mocker.MagicMock()
+    mocked_function = mocker.patch.object(ZendeskApiCallHelper, "get_ticket", return_value=mock_response)
+    response = get_ticket(
+        {'resource': '/tickets/{id}', 'path': '/tickets/1', 'httpMethod': 'GET',
+         'headers': {'Accept': '*/*', 'Accept-Encoding': 'gzip, deflate, br', 'Accept-Language': 'en-US,en;q=0.9',
+                     'CloudFront-Forwarded-Proto': 'https', 'CloudFront-Is-Desktop-Viewer': 'true',
+                     'CloudFront-Is-Mobile-Viewer': 'false', 'CloudFront-Is-SmartTV-Viewer': 'false',
+                     'CloudFront-Is-Tablet-Viewer': 'false', 'CloudFront-Viewer-Country': 'US',
+                     'Host': 'faq1slxbph.execute-api.us-east-1.amazonaws.com', 'origin': 'http://localhost:3000',
+                     'Referer': 'http://localhost:3000/',
+                     'sec-ch-ua': '" Not;A Brand";v="99", "Google Chrome";v="91", "Chromium";v="91"',
+                     'sec-ch-ua-mobile': '?0', 'sec-fetch-dest': 'empty', 'sec-fetch-mode': 'cors',
+                     'sec-fetch-site': 'cross-site',
+                     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36',
+                     'Via': '2.0 27f5831be5a9ad411fca9c84fe627bdc.cloudfront.net (CloudFront)',
+                     'X-Amz-Cf-Id': '31c5JvO6KQbQjpWEdc5Rey0VW6PvAO8g1OhX6YSkVV4kZZseAeXoEw==',
+                     'X-Amzn-Trace-Id': 'Root=1-6101a0bb-31d3485b54b5fd8866015641',
+                     'X-Forwarded-For': '76.124.168.93, 52.46.46.111', 'X-Forwarded-Port': '443',
+                     'X-Forwarded-Proto': 'https'},
+         'multiValueHeaders': {'Accept': ['*/*'], 'Accept-Encoding': ['gzip, deflate, br'],
+                               'Accept-Language': ['en-US,en;q=0.9'], 'CloudFront-Forwarded-Proto': ['https'],
+                               'CloudFront-Is-Desktop-Viewer': ['true'], 'CloudFront-Is-Mobile-Viewer': ['false'],
+                               'CloudFront-Is-SmartTV-Viewer': ['false'], 'CloudFront-Is-Tablet-Viewer': ['false'],
+                               'CloudFront-Viewer-Country': ['US'],
+                               'Host': ['faq1slxbph.execute-api.us-east-1.amazonaws.com'],
+                               'origin': ['http://localhost:3000'], 'Referer': ['http://localhost:3000/'],
+                               'sec-ch-ua': ['" Not;A Brand";v="99", "Google Chrome";v="91", "Chromium";v="91"'],
+                               'sec-ch-ua-mobile': ['?0'], 'sec-fetch-dest': ['empty'], 'sec-fetch-mode': ['cors'],
+                               'sec-fetch-site': ['cross-site'], 'User-Agent': [
+                 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36'],
+                               'Via': ['2.0 27f5831be5a9ad411fca9c84fe627bdc.cloudfront.net (CloudFront)'],
+                               'X-Amz-Cf-Id': ['31c5JvO6KQbQjpWEdc5Rey0VW6PvAO8g1OhX6YSkVV4kZZseAeXoEw=='],
+                               'X-Amzn-Trace-Id': ['Root=1-6101a0bb-31d3485b54b5fd8866015641'],
+                               'X-Forwarded-For': ['76.124.168.93, 52.46.46.111'], 'X-Forwarded-Port': ['443'],
+                               'X-Forwarded-Proto': ['https']}, 'queryStringParameters': None,
+         'multiValueQueryStringParameters': None, 'pathParameters': {'id': '1'}, 'stageVariables': None,
+         'requestContext': {'resourceId': 'ciwo05', 'resourcePath': '/tickets/{id}', 'httpMethod': 'GET',
+                            'extendedRequestId': 'DMYNTHvQIAMFV7g=', 'requestTime': '28/Jul/2021:18:23:55 +0000',
+                            'path': '/dev/tickets/1', 'accountId': '689157321639', 'protocol': 'HTTP/1.1',
+                            'stage': 'dev', 'domainPrefix': 'faq1slxbph', 'requestTimeEpoch': 1627496635393,
+                            'requestId': '5d8d4dbd-d712-40a6-8a64-a6a1f5f6450d',
+                            'identity': {'cognitoIdentityPoolId': None, 'accountId': None, 'cognitoIdentityId': None,
+                                         'caller': None, 'sourceIp': '76.124.168.93', 'principalOrgId': None,
+                                         'accessKey': None, 'cognitoAuthenticationType': None,
+                                         'cognitoAuthenticationProvider': None, 'userArn': None,
+                                         'userAgent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36',
+                                         'user': None}, 'domainName': 'faq1slxbph.execute-api.us-east-1.amazonaws.com',
+                            'apiId': 'faq1slxbph'}, 'body': None, 'isBase64Encoded': False}, {}
+    )
+    assert mocked_function.call_count == 1
+    assert response == {'statusCode': 500, 'headers': {'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Credentials': True}, 'body': '"Object of type MagicMock is not JSON serializable"'}
